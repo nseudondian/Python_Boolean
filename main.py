8e519b52ccb7258ff980a7a5874079e62f07c157
Nseabasi Udondian <nseabasiudondian@gmail.com>
2023-07-14 16:51:50 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,118 @@
+# Boolean : True or False. 
+
+# Creating Boolean variables
+
+# is_true = True
+
+# is_false = False
+
+
+# print (type(is_true))
+
+# print (type(is_false)) 
+
+# Comparison Operators
+# Comparison operators are used to compare values and return a Boolean result.
+
+# x = 5
+# y = 5
+
+# == (equal to): Returns True if the values on both sides of the operator are equal. 
+
+# print(x == y) 
+# print(5 == 5)
+
+
+
+
+ 
+# channel = "Learn-In" 
+
+# print(x==y)
+# print(5==10)
+
+
+
+# != (not equal to): Returns True if the values on both sides of the operator are not equal.  
+
+# x = 5
+# y = 5
+
+# print(x != y)  
+
+
+
+
+
+
+
+
+
+
+# > (greater than): Returns True if the value on the left is greater than the value on the right.  
+
+# x = 5
+# y = 10
+
+# print(x > y) 
+# print(5 > 10) 
+
+# print(y > x)
+# print(10 > 5)
+
+
+
+
+
+# < (less than): Returns True if the value on the left is less than the value on the right. 
+
+# x = 5
+# y = 10
+
+# print(x < y)   
+# print(y < x)
+
+
+# >= (greater than or equal to): Returns True if the value on the left is greater than or equal to the value on the right. 
+ 
+# x = 5
+# y = 5
+
+# # print(x >= y)   
+# # print (5 >= 10)
+
+# print(y >= x)
+
+
+# <= (less than or equal to): Returns True if the value on the left is less than or equal to the value on the right.  
+
+# x = 15.34
+# y = 5.89
+
+# print(x >= y) 
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Comparing Strings
+# ord() function: returns the Unicode code point of a given character.
+
+
+# print("A" < "a")
+
+
+
+print(ord("A"))  
+print(ord("a"))  
+
